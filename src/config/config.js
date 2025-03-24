7d2f4b25cf2c4e557850c5c1423bc862e1ea1834
khairililmi2468gmailcom <khairililmi2468@gmail.com>
2025-03-24 23:54:59 +0700

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,50 @@
+const config = {
+  data: {
+    title: "Pernikahan Mukhibuddin & Eka Ramadani",
+    description:
+      "Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.",
+    groomName: "Mukhibuddin",
+    brideName: "Eka Ramadani",
+    parentGroom: "Bapak Groom & Ibu Groom",
+    parentBride: "Bapak Bride & Ibu Bride",
+    date: "2025-04-07",
+    maps_url: "https://maps.app.goo.gl/TapQ78k25LZussXw9",
+    maps_embed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d819.4339866523532!2d96.75217101039068!3d4.823152988828994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3038bd78a7e77ae5%3A0x61e759e1eaa179f8!2sReronga%2C%20Kec.%20Gajah%20Putih%2C%20Kabupaten%20Bener%20Meriah%2C%20Aceh!5e1!3m2!1sid!2sid!4v1742832962119!5m2!1sid!2sid",
+    time: "09.00 s.d Selesai",
+    location: "Dusun Sumberjo A, Desa Reronga, Kecamatan Gajah Putih, Kabupaten Bener Meriah",
+    address: "Jl. Bireun - Takengon Km.65 ",
+    ogImage: "/images/og-image.jpg",
+    favicon: "/images/favicon.ico",
+    agenda: [
+      {
+        title: "Resepsi Nikah",
+        date: "2025-04-07",
+        startTime: "16:16",
+        endTime: "17:30",
+        location: "Dusun Sumberjo A, Desa Reronga, Kecamatan Gajah Putih, Kabupaten Bener Meriah",
+        address: "Jl. Bireun - Takengon Km.65",
+      }
+    ],
+    audio: {
+      src: "/audio/fulfilling-humming.mp3",
+      title: "Fulfilling Humming",
+      autoplay: true,
+      loop: true
+    },
+    banks: [
+      {
+        bank: "Bank Central Asia",
+        accountNumber: "1234567890",
+        accountName: "Mukhibuddin",
+      },
+      {
+        bank: "Bank Mandiri",
+        accountNumber: "0987654321",
+        accountName: "Eka Ramadani",
+      }
+    ]
+  }
+};
+
+export default config;
