0bbff79864268e1d97589a59299b998191ac283c
Khairil Ilmi <84059036+khairililmi2468gmailcom@users.noreply.github.com>
2025-03-25 13:14:56 +0700

    Update MainContent.jsx

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -11,7 +11,7 @@ export default function MainContent() {
             <Hero />
             <Events />
             <Location />
-            <Gifts />
+{/*             <Gifts /> */}
             <Wishes />
         </>
     )
